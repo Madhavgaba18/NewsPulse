84594c454cd85844001433f99044da220fefba12
Madhavgaba18 <madhavgaba42@gmail.com>
2024-04-21 07:19:00 +0530

    Tour steps updated

--- src/Components/Steps.js
+++ src/Components/Steps.js
@@ -1,38 +1,38 @@
 export const steps = [
   {
     selector: ".First-step",
-    content: "this is the First step",
+    content: "this is the Name of our Website",
   },
   {
     selector: ".Second-step",
-    content: "this is the Second step",
+    content: "this is the Home Page Button",
   },
   {
     selector: ".Third-step",
-    content: "this is the third step",
+    content: "this is the Business Page Button",
   },
   {
     selector: ".Fourth-step",
-    content: ".this is the Fourth step",
+    content: "this is the Entertainment Page Button",
   },
   {
     selector: ".Fifth-step",
-    content: "this is the Fifth step",
+    content: "this is the Health Page Button",
   },
   {
     selector: ".Sixth-step",
-    content: "this is the Sixth step",
+    content: "this is the Science Page Button",
   },
   {
     selector: ".Seventh-step",
-    content: "this is the Seventh step",
+    content: "this is the Sports Page Button",
   },
   {
     selector: ".Eighth-step",
-    content: "this is the Eighth step",
+    content: "this is the Technology Page Button",
   },
   {
     selector: ".Ninth-step",
-    content: "this is the Ninth step",
+    content: "this is the Heading",
   },
 ];
